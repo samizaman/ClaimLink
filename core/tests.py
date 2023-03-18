c2ef979fefd22bca01032211b50814e96104f3be
Sami <sxmizaman@gmail.com>
2023-03-18 06:02:45 +0400

    add testcase for "home" view

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_view_success(self):
+        # Issue a GET request to the home page
+        response = self.client.get(reverse("home"))
+
+        # Check if the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the correct template is used to render the response
+        self.assertTemplateUsed(response, "home.html")
