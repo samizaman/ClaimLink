938ffbc11dfb77ba8cb1ea12463dcdc4b5501ff1
Sami <sxmizaman@gmail.com>
2023-04-28 07:31:57 +0400

    add claim details testcase

--- core/tests/tests_views.py
+++ core/tests/tests_views.py
@@ -62,3 +62,43 @@ class PersonalDetailsViewTests(TestCase):
         form = response.context.get("form")
         self.assertIsInstance(form, PersonalDetailsForm)
         self.assertFalse(form.is_valid())
+
+
+class ClaimDetailsViewTests(TestCase):
+    def test_claim_details_view_uses_correct_template(self):
+        response = self.client.get(reverse("claim_details"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "claim_details.html")
+
+    def test_claim_details_view_form_submission(self):
+        response = self.client.post(
+            reverse("claim_details"),
+            {
+                "date_of_loss": "2022-01-01",
+                "country_of_incident": "US",  # <-- Use the correct value here
+                "claim_amount": "1000",
+                "claim_amount_currency": "USD",
+                "description_of_loss": "Lost baggage during travel.",
+            },
+        )
+
+        if response.status_code != 302:
+            form = response.context.get("form")
+            print("Form errors:", form.errors)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("coverage_items_selection"))
+
+    def test_claim_details_view_form_submission_with_errors(self):
+        response = self.client.post(
+            reverse("claim_details"),
+            {
+                "date_of_loss": "",
+                "claim_amount": "1000",
+                "description": "Lost baggage during travel.",
+            },
+        )
+
+        form = response.context.get("form")
+        self.assertFalse(form.is_valid())
+        self.assertEqual(response.status_code, 200)
