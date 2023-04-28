4c43bd5d1e32303671f7439e49ee78e270726e92
Sami <sxmizaman@gmail.com>
2023-04-28 07:15:21 +0400

    add tests for personal details

--- core/tests/tests_views.py
+++ core/tests/tests_views.py
@@ -1,6 +1,8 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from core.forms import PersonalDetailsForm
+
 
 class HomeViewTestCase(TestCase):
     def setUp(self):
@@ -15,3 +17,48 @@ class HomeViewTestCase(TestCase):
 
         # Check if the correct template is used to render the response
         self.assertTemplateUsed(response, "home.html")
+
+
+class PersonalDetailsViewTests(TestCase):
+    def test_personal_details_view_uses_correct_template(self):
+        response = self.client.get(reverse("personal_details"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "personal_details.html")
+
+    def test_personal_details_view_form_submission(self):
+        response = self.client.post(
+            reverse("personal_details"),
+            {
+                "first_name": "John",
+                "last_name": "Doe",
+                "name": "John Doe",
+                "dob": "1990-01-01",
+                "email": "john.doe@example.com",
+                "phone_number": "1234567890",
+                "gender": "M",
+            },
+        )
+
+        if response.status_code != 302:
+            form = response.context.get("form")
+            print("Form errors:", form.errors)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("claim_details"))
+
+    def test_personal_details_view_form_invalid_submission(self):
+        response = self.client.post(
+            reverse("personal_details"),
+            {
+                "first_name": "John",
+                "last_name": "Doe",
+                "dob": "INVALID_DATE",
+                "email": "INVALID_EMAIL",
+                "phone": "+1 234 567 8900",
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        form = response.context.get("form")
+        self.assertIsInstance(form, PersonalDetailsForm)
+        self.assertFalse(form.is_valid())
